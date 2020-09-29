d491ed4a0fe872d4396fd981954758684fe17a8f
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2020-09-29 18:22:32 +0300

    Cleanup

--- library/Two.ts
+++ library/Two.ts
@@ -1,4 +1,4 @@
-import Two from 'two';
+import * as Two from 'two.js';
 
 const PI = Math.PI;
 const HALF_PI = PI / 2;
@@ -54,11 +54,6 @@ const createShape = (shape: Two.Path, props: ShapeProps) => {
   shape.opacity = opacity;
   shape.rotation = rotate;
 
-  if (strokeDasharray) {
-    shape.dashes[0] = strokeDasharray[0];
-    shape.dashes[1] = strokeDasharray[1];
-  }
-
   return shape;
 };
 
@@ -72,61 +67,3 @@ export const createText = (text: string, props: ShapeProps & { x: number; y: num
   return new Two.Text(text, props.x, props.y, props);
 };
 
-const arcsToAnchors = (arcs: ArcProps[], closed?: boolean) => {
-  const R = Two.Resolution * 3;
-  const anchors = Array
-    .from({ length: (R * arcs.length) })
-    .map(() => new Two.Anchor());
-
-  for (let i = 0; i < arcs.length; i++) {
-
-    const { a1, a2, cx, cy, radius } = arcs[i];
-
-    for (let j = 0; j < R; j++) {
-      const anchorIndex = i * R + j;
-      const anchor = anchors[anchorIndex];
-      const theta = (j / (R - 1)) * (a2 - a1) + a1;
-
-      if (i === 0 && j === 0) {
-        anchor.command = Two.Commands.move;
-      } else {
-        anchor.command = Two.Commands.curve;
-      }
-
-      anchor.x = cx + (radius * Math.cos(theta));
-      anchor.y = cy + (radius * Math.sin(theta));
-
-      if (anchor.controls) {
-        anchor.controls.left.clear();
-        anchor.controls.right.clear();
-      }
-
-      if (anchor.command === Two.Commands.curve) {
-        const amp = (radius * ((a2 - a1) / R) / PI);
-
-        if (j !== 0) {
-          anchor.controls.left.x = amp * Math.cos(theta - HALF_PI);
-          anchor.controls.left.y = amp * Math.sin(theta - HALF_PI);
-        }
-
-        if (j !== R - 1) {
-          anchor.controls.right.x = amp * Math.cos(theta + HALF_PI);
-          anchor.controls.right.y = amp * Math.sin(theta + HALF_PI);
-        }
-      }
-    }
-  }
-
-  if (closed) {
-    anchors[anchors.length - 1].x = anchors[0].x;
-    anchors[anchors.length - 1].y = anchors[0].y;
-  }
-
-  return anchors;
-};
-
-export const createPolygonArc = (props: ShapeProps & { arcs: ArcProps[] }) => {
-  return createShape(
-    new Two.Path(arcsToAnchors(props.arcs, true), true, false, true),
-  props);
-};
