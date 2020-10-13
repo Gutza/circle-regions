cb6646973936055475527405fd06b671bd0fd648
Bogdan Stancescu <bogdan@moongate.ro>
2020-10-13 08:32:35 +0300

    Still buggy, but refactored for clarity

--- src/RegionEngine.ts
+++ src/RegionEngine.ts
@@ -129,33 +129,78 @@ export class RegionEngine {
         }
     }
     
-    private _computeLoops = (): GraphLoop[] => {
-        // TODO: Sweep line
+    // (1/5)
+    private _removeDirtyNodesVertices = () => {
+        let dirtyCircles = this._circles.filter(circle => circle.isDirty);
+        console.log("_resetDirtyCaches on", dirtyCircles.length, "circles");
+
+        // Affected nodes are all nodes which include dirty circles.
+        const affectedNodes = this._nodes.filter(node => node.tangencyGroups.some(tanGroup => tanGroup.some(tge => dirtyCircles.includes(tge.circle))));
+
+        // Remove the dirty circles from all affected nodes.
+        affectedNodes.forEach(node => node.removeCircles(dirtyCircles));
+
+        console.log("Node count before filtering", this._nodes.length, "(affected", affectedNodes.length, ")");
+        this._nodes = this._nodes.filter((node): boolean => {
+            if (!affectedNodes.includes(node)) {
+                return true;
+            }
+
+            this._circles.forEach(circle => {
+                circle.removeVertexByNode(node);
+            });
+
+            return node.isValid();
+        });
+        console.log("Node count after filtering", this._nodes.length);
+    }
+
+    /**
+     * (2/5)
+     * This is an important milestone in the process.
+     * 
+     * _computeIntersections() sets parents and children, and creates nodes by
+     * intersecting dirty circles with all other circles. When an intersection
+     * is found between a dirty circle and a clean circle, the clean circle
+     * gets contaminated -- its vertices need re-sorting, its edges deleted and re-created,
+     * and all adjacent regions need to be deleted and re-computed.
+     * 
+     * As such, dirty vertex and node cleanup must be performed before computing the
+     * intersections, while edge and region cleanup must be done after computing them.
+     * 
+     * This also means the set of dirty circles potentially increases in size during
+     * this step, therefore:
+     * (1) The notion of a circle being dirty means different things before and after this step;
+     * (2) The list of dirty circles must be recomputed after this operation (do not cache it across this step).
+     */
+    private _computeIntersections = () => {
+        // TODO: Implement sweep line x 2
         for (let i = 0; i < this._circles.length-1; i++) {
             const c1 = this._circles[i];
-            console.log("in computeLoops, i="+i);
             for (let j = i+1; j < this._circles.length; j++) {
-                console.log("in computeLoops, j="+j);
                 const c2 = this._circles[j];
-                console.log("computeLoops on", c1.id, "and", c2.id);
                 if (!c1.isDirty && !c2.isDirty) {
-                    console.log("computeLoops: both are clean");
                     continue;
                 }
+
                 if (!c1.boundingBoxOverlap(c2)) {
-                    console.log("computeLoops: bboxes don't overlap");
                     continue;
                 }
+                
                 intersectCircles(this, c1, c2);
             }
         }
+    }
 
+    // (3/5)
+    private _rebuildDirtyEdges = () => {
         let dirtyCircles = this._circles.filter(circle => circle.isDirty);
-        console.log("Reprocessing vertices for", dirtyCircles.length, "dirty circles, out of", this._circles.length, "total");
-        dirtyCircles.forEach(circle => {
-            const nodes = this._nodes.filter(node => node.tangencyGroups.some(tg => tg.some(tge => tge.circle == circle)));
+        this._edges = this._edges.filter(edge => !dirtyCircles.includes(edge.circle));
+        console.log("Adding vertices to", dirtyCircles.length, "dirty circles, out of", this._circles.length, "total");
+        dirtyCircles.forEach(dirtyCircle => {
+            const nodes = this._nodes.filter(node => node.tangencyGroups.some(tg => tg.some(tge => tge.circle == dirtyCircle)));
             nodes.forEach(node => {
-                circle.addVertex(new CircleVertex(node, circle));
+                dirtyCircle.addVertex(new CircleVertex(node, dirtyCircle));
             });
         });
 
@@ -168,13 +213,17 @@ export class RegionEngine {
             for (let i = 0; i < circle.vertices.length; i++) {
                 // This will add a single edge for circles which have a single tangency point; that's ok
                 const newEdge = new GraphEdge(circle, circle.vertices[i].node, circle.vertices[i+1] ? circle.vertices[i+1].node : circle.vertices[0].node, i);
+                console.log("Adding edge to circle", circle.id,"(existing", this._edges.length, "total)");
                 this._edges.push(newEdge);
                 newEdge.node1.addEdge(newEdge);
                 newEdge.node2.addEdge(newEdge);
             }
             circle.isDirty = false;
         });
+    }
 
+    // (4/5)
+    private _computeLoops = (): GraphLoop[] => {
         const loops: GraphLoop[] = [];
 
         while (true) {
@@ -207,35 +256,7 @@ export class RegionEngine {
         return loops;
     }
 
-    private _resetDirtyCaches = () => {
-        const dirtyCircles = this._circles.filter(circle => circle.isDirty);
-        console.log("_resetDirtyCaches on", dirtyCircles.length, "circles");
-
-        // Affected nodes are all nodes which include dirty circles.
-        const affectedNodes = this._nodes.filter(node => node.tangencyGroups.some(tanGroup => tanGroup.some(tge => dirtyCircles.includes(tge.circle))));
-
-        // Remove the dirty circles from all affected nodes.
-        affectedNodes.forEach(node => node.removeCircles(dirtyCircles));
-
-        console.log("Node count before filtering", this._nodes.length, "(affected", affectedNodes.length, ")");
-        this._nodes = this._nodes.filter((node): boolean => {
-            if (!affectedNodes.includes(node)) {
-                return true;
-            }
-
-            this._circles.forEach(circle => {
-                circle.removeVertexByNode(node);
-            });
-
-            return node.isValid();
-        });
-        console.log("Node count after filtering", this._nodes.length);
-
-        console.log("Edge count before filtering", this._edges.length);
-        this._edges = this._edges.filter(edge => !dirtyCircles.includes(edge.circle));
-        console.log("Edge count after filtering", this._edges.length);
-    }
-
+    // (5/5)
     private _computeRegions = (loops: GraphLoop[]): CircleRegion[] => {
         const newRegions: CircleRegion[] = loops.map(loop => {
             const arcs: CircleArc[] = [];
@@ -272,14 +293,23 @@ export class RegionEngine {
             return this._regions;
         }
 
-        this._resetDirtyCaches();
+        //  (1/5)
+        this._removeDirtyNodesVertices();
         
         console.log("Edge count at region time", this._edges.length);
 
+        // (2/5)
+        this._computeIntersections();
+
+        // (3/5)
+        this._rebuildDirtyEdges();
+
+        // (4/5)
         const loops = this._computeLoops();
 
         console.log("Loop count", loops.length);
 
+        // (5/5)
         this._regions = this._computeRegions(loops);
 
         console.log("Computed regions");
