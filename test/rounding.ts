3aa4b951bf9cd76edd8513afe80e9640a073bd2c
Bogdan Stancescu <gutza@moongate.ro>
2020-11-13 01:26:33 +0200

    New bug!

--- test/rounding.ts
+++ test/rounding.ts
@@ -8,7 +8,7 @@ const expCircle = (exp: number, cx: number, cy: number, r: number): Circle => {
     return new Circle(new Point(cx * expNum, cy * expNum), r * expNum);
 }
 
-const roundingWorkbench = (label: string, roundingSize: ERoundingSize, minExp: number, maxExp: number): void => {
+const roundingWorkbenchSimple = (label: string, roundingSize: ERoundingSize, minExp: number, maxExp: number): void => {
     setRounding(roundingSize);
     describe(label + " size circle rounding", () => {
         for (var exp = minExp; exp <= maxExp; exp++)
@@ -28,6 +28,32 @@ const roundingWorkbench = (label: string, roundingSize: ERoundingSize, minExp: n
     });
 }
 
-roundingWorkbench("Default", ERoundingSize.default, -5, 11);
-roundingWorkbench("Small", ERoundingSize.small, -8, 7);
-roundingWorkbench("Large", ERoundingSize.large, -2, 14);
+roundingWorkbenchSimple("Default", ERoundingSize.default, -5, 11);
+roundingWorkbenchSimple("Small", ERoundingSize.small, -8, 7);
+roundingWorkbenchSimple("Large", ERoundingSize.large, -2, 14);
+
+const roundingWorkbenchExtra = (label: string, roundingSize: ERoundingSize, minExp: number, maxExp: number): void => {
+    const totalSteps = 17;
+    const angleStep = Math.PI*2/totalSteps;
+    const rad = 2;
+    setRounding(roundingSize);
+    describe(label + " size circle rounding", () => {
+        for (var angle = 0; angle < 2 * Math.PI; angle += angleStep) {
+            for (var exp = minExp; exp <= maxExp; exp++)
+            {
+                const engine = new RegionEngine();
+                for (var regionAngle = 0; regionAngle < 2 * Math.PI; regionAngle += Math.PI/2)  {
+                    engine.addCircle(expCircle(exp, rad * Math.cos(angle+regionAngle), rad * Math.sin(angle+regionAngle), rad));
+                }
+                engine.regions;
+                const nodeCount = engine.nodes.length;
+                const testExp = exp;
+                it (label + " rounding should work at 10^" + testExp, () => {
+                    assert.strictEqual(nodeCount, 5, "Rounding fails for exp=" + testExp);
+                });
+            }
+        }
+    });
+}
+
+roundingWorkbenchExtra("Default", ERoundingSize.default, -5, 11);
