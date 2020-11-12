00661f59bc90a15f52892cc2f4c0a5ef7a5f8e70
Bogdan Stancescu <gutza@moongate.ro>
2020-11-13 01:02:53 +0200

    Mature testing workbench for rounding

--- test/rounding.ts
+++ test/rounding.ts
@@ -8,56 +8,26 @@ const expCircle = (exp: number, cx: number, cy: number, r: number): Circle => {
     return new Circle(new Point(cx * expNum, cy * expNum), r * expNum);
 }
 
-setRounding(ERoundingSize.default);
-describe("Testing default circles", () => {
-    for (var exp = -5; exp <= 11; exp++)
-    {
-        const engine = new RegionEngine();
-        engine.addCircle(expCircle(exp, -Math.SQRT2, -Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, +Math.SQRT2, +Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, -Math.SQRT2, +Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, +Math.SQRT2, -Math.SQRT2, 2));
-        engine.regions;
-        const nodeCount = engine.nodes.length;
-        const testExp = exp;
-        it ("Default rounding should work well across a wide gamut of scales", () => {
-            assert.strictEqual(nodeCount, 5, "Rounding fails for exp=" + testExp);
-        });
-    }
-});
+const roundingWorkbench = (label: string, roundingSize: ERoundingSize, minExp: number, maxExp: number): void => {
+    setRounding(roundingSize);
+    describe(label + " size circle rounding", () => {
+        for (var exp = minExp; exp <= maxExp; exp++)
+        {
+            const engine = new RegionEngine();
+            engine.addCircle(expCircle(exp, -Math.SQRT2, -Math.SQRT2, 2));
+            engine.addCircle(expCircle(exp, +Math.SQRT2, +Math.SQRT2, 2));
+            engine.addCircle(expCircle(exp, -Math.SQRT2, +Math.SQRT2, 2));
+            engine.addCircle(expCircle(exp, +Math.SQRT2, -Math.SQRT2, 2));
+            engine.regions;
+            const nodeCount = engine.nodes.length;
+            const testExp = exp;
+            it (label + " rounding should work at 10^" + testExp, () => {
+                assert.strictEqual(nodeCount, 5, "Rounding fails for exp=" + testExp);
+            });
+        }
+    });
+}
 
-setRounding(ERoundingSize.small);
-describe("Testing small circles", () => {
-    for (var exp = -8; exp <= 7; exp++)
-    {
-        const engine = new RegionEngine();
-        engine.addCircle(expCircle(exp, -Math.SQRT2, -Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, +Math.SQRT2, +Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, -Math.SQRT2, +Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, +Math.SQRT2, -Math.SQRT2, 2));
-        engine.regions;
-        const nodeCount = engine.nodes.length;
-        const testExp = exp;
-        it ("Small rounding should work well across a wide gamut of scales", () => {
-            assert.strictEqual(nodeCount, 5, "Rounding fails for exp=" + testExp);
-        });
-    }
-});
-
-setRounding(ERoundingSize.large);
-describe("Testing large circles", () => {
-    for (var exp = -2; exp <= 14; exp++)
-    {
-        const engine = new RegionEngine();
-        engine.addCircle(expCircle(exp, -Math.SQRT2, -Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, +Math.SQRT2, +Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, -Math.SQRT2, +Math.SQRT2, 2));
-        engine.addCircle(expCircle(exp, +Math.SQRT2, -Math.SQRT2, 2));
-        engine.regions;
-        const nodeCount = engine.nodes.length;
-        const testExp = exp;
-        it ("Large rounding should work well across a wide gamut of scales", () => {
-            assert.strictEqual(nodeCount, 5, "Rounding fails for exp=" + testExp);
-        });
-    }
-});
+roundingWorkbench("Default", ERoundingSize.default, -5, 11);
+roundingWorkbench("Small", ERoundingSize.small, -8, 7);
+roundingWorkbench("Large", ERoundingSize.large, -2, 14);
