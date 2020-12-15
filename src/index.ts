73086d85bcdcc248d1b76eecb6f0a5e805702150
Bogdan Stancescu <gutza@moongate.ro>
2020-12-15 21:27:12 +0200

    Moar exports

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,5 @@
 export { Circle } from "./geometry/Circle";
 export { RegionEngine } from "./RegionEngine";
 export { Point } from "./geometry/Point";
+export { EDrawableEventType, ERegionType, IPoint } from "./Types";
+export { ArcPolygon } from "./geometry/ArcPolygon";
