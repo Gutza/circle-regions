54d2a2d2ebad511236ac63e68adfa733e007dd30
Bogdan Stancescu <gutza@moongate.ro>
2020-12-22 21:47:21 +0200

    Moved main TODOs to dedicated list

---
+++ TODO.md
@@ -0,0 +1,10 @@
+ - XH: Investigate documentation labels (lazy, cached, computationally cheap, etc)
+ - H: Documentation for public methods/classes
+ - H: Convert generic errors to project-specific errors
+ - M: Region "centroid" (largest inscribed circle)
+ - M: Region persistence (optional; based on parents and neighbors)
+ - M: Region change events (depends on region persistence)
+ - L: Performance benchmarks
+ - L: Coveralls
+ - XL: Rotating cardioid test
+ 
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-/*
- TODO/L: Benchmarks
-
- TODO/L: Coverall
-
- TODO/L: Rotating cardioid test
-
- TODO/M: Region "centroid" (largest inscribed circle)
-
- TODO/M: Region persistence (optional; based on parents and neighbors)
-
- TODO/M: Region change events (depends on region persistence)
-
- TODO/L: Circles as inner/outer contours (tangent circles)
-
- TODO/H: Documentation
-
- TODO/XH: Investigate documentation labels (lazy, cached, computationally cheap, etc)
-
- TODO/H: Convert generic errors to project-specific errors
- */
-
 export { Circle } from "./geometry/Circle";
 export { RegionEngine } from "./RegionEngine";
 export { Point } from "./geometry/Point";
