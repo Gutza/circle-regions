6767f4282dedfe6f3dfbbd5808a0bdc552706ed6
Bogdan Stancescu <gutza@moongate.ro>
2020-12-29 09:58:59 +0200

    Started working on helpers (Bezier)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { RegionEngine } from "./RegionEngine";
 export { Point } from "./geometry/Point";
 export { EDrawableEventType, ERegionType, IPoint } from "./Types";
 export { ArcPolygon } from "./geometry/ArcPolygon";
+export { BezierHelper } from "./geometry/helpers/BezierHelper";
