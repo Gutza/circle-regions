ef0dce96d4fcbf11e8caba2f2885f97b05da81b1
Bogdan Stancescu <gutza@moongate.ro>
2021-01-02 21:41:28 +0200

    Fixed mistaken order

--- src/geometry/helpers/helperHelpers.ts
+++ src/geometry/helpers/helperHelpers.ts
@@ -7,8 +7,8 @@ export function makeSafeRenderingArc(arc: CircleArc): CircleArc {
     }
 
     if (arc.isClockwise) {
-        return new CircleArc(arc.circle, arc.startAngle, arc.endAngle + TWO_PI, arc.startPoint, arc.endPoint, true);
+        return new CircleArc(arc.circle, arc.startAngle + TWO_PI, arc.endAngle, arc.startPoint, arc.endPoint, true);
     } else {
-        return new CircleArc(arc.circle, arc.startAngle + TWO_PI, arc.endAngle, arc.startPoint, arc.endPoint, false);
+        return new CircleArc(arc.circle, arc.startAngle, arc.endAngle + TWO_PI, arc.startPoint, arc.endPoint, false);
     }
 }
